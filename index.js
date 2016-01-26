3b014a2452e4faae51cd9852eb61f1cd6c2aedd5
CodeMeow <codemeow@yahoo.com>
2016-01-26 16:30:10 +0800

    oh, tired

--- index.js
+++ index.js
@@ -22,12 +22,10 @@ app.on('window-all-closed', function(){
 var local = this;
 
 electron.ipcMain.on('mqtt-connect', function(event, arg){
-    local.log = function(logstr){
-        event.sender.send('log', logstr);
+
+    if(!!local.log){
+        local.log('Begin connecting');
     }
-
-    local.log('Begin connecting');
-
     var options = {
         'host': arg.ip_address,
         'port': arg.port,
@@ -49,17 +47,23 @@ electron.ipcMain.on('mqtt-connect', function(event, arg){
     client = mqtt.connect(options);
     client.on('connect', function(){
         if(!!local.log){
-          local.log('Connect successfully');
+            local.log('Connect successfully');
+        }
+    });
+    client.on('error', function(err){
+        if(!!local.log){
+            local.log(err);
         }
     });
     client.on('close', function(){
-        local.log('The connection is closed(from close callback)');
+        if(!!local.log){
+            local.log('The connection is closed(from event "close")');
+        }
     });
     client.on('message', function(topic, payload){
-        event.sender.send('message', {
-            'topic': topic,
-            'payload': payload
-        });
+        if(!!local.log){
+            local.log('Received "' + payload + '" from "' + topic + '"');
+        }
     });
 
 });
@@ -69,52 +73,83 @@ electron.ipcMain.on('mqtt-disconnect', function(event){
         local.log('Disconnect');
     }
     if(!!client){
-      client.end();
+        client.end(function(){
+            if(!!local){
+              local.log('The connection is closed(from callback "end")');
+            }
+        });
+    } else {
+        if(!!local.log){
+            local.log('The connection is not established');
+        }
     }
 });
 
 electron.ipcMain.on('mqtt-subscribe', function(event, arg){
     if(!!local.log){
-        local.log('Subscribe');
+        local.log('Subscribe(Topic is "' + arg.topic + '")');
     }
     if(!!client){
-        client.subscribe(arg, function(err, granted){
+        client.subscribe(arg.topic, {'qos': parseInt(arg.qos)}, function(err, granted){
             if(!!local.log){
-                local.log('Subscription success(from subscribe callback)');
+                for(let _ in granted){
+                    local.log('Subscription success(Topic is "' +
+                      granted[_].topic + '" and QoS is ' + granted[_].qos + ')');
+                }
             }
         });
+    } else {
+        if(!!local.log){
+            local.log('The connection is not established');
+        }
     }
 });
 
 electron.ipcMain.on('mqtt-unsubscribe', function(event, arg){
     if(!!local.log){
-        local.log('Unsubscribe');
+        local.log('Unsubscribe(Topic is "' + arg.topic + '")');
     }
     if(!!client){
-      client.unsubscribe(arg, function(err, granted){
+      client.unsubscribe(arg.topic, function(err, granted){
           if(!!local.log){
-              local.log('Unsubscription success(from unsubscribe callback)');
+              local.log('Unsubscription success(from callback "unsubscribe")');
           }
       });
+    } else {
+        if(!!local.log){
+            local.log('The connection is not established');
+        }
     }
 });
 
 electron.ipcMain.on('mqtt-publish', function(event, arg){
     if(!!local.log){
-          local.log('Publish "' + arg.message + '" to "' + arg.topic + '"');
+          local.log('Publish "' + arg.message + '" to "' + arg.topic +
+            '"(QoS is ' + arg.qos + ' and Retain is ' + arg.retain + ')');
     }
     if(!!client){
-      client.publish(arg.topic, arg.message, function(err, granted){
+      client.publish(arg.topic, arg.message, {
+          'qos': parseInt(arg.qos),
+          'retain': arg.retain
+      }, function(err, granted){
           if(!!local.log){
-                local.log('Publish success(from publish callback)');
+                local.log('Publish success(from callback "publish")');
           }
       });
+    } else {
+        if(!!local.log){
+            local.log('The connection is not established');
+        }
     }
 });
 
 app.on('ready', function(){
-    mainWindow = new BrowserWindow({width: 600, height: 800});
+    mainWindow = new BrowserWindow({width: 600, height: 900});
     mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    local.log = function(logstr){
+        mainWindow.webContents.send('log', logstr);
+    }
 
     //mainWindow.webContents.openDevTools();
 
